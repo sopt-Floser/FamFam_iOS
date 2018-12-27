1ddc509c840ecb905b28f4c2684ca3b475bf2ec8
ArtificIn <cisspmit@naver.com>
2018-12-27 17:54:58 +0900

    [다연] 홈 조정중

---
+++ flower/Home/Family/FamilyCell.swift
@@ -0,0 +1,15 @@
+//
+//  FamilyCell.swift
+//  flower
+//
+//  Created by 성다연 on 2018. 12. 27..
+//  Copyright © 2018년 성다연. All rights reserved.
+//
+
+import UIKit
+
+class FamilyCell: UICollectionViewCell {
+    @IBOutlet weak var familyImage: UIImageView!
+    @IBOutlet weak var familyName: UILabel!
+    
+}
