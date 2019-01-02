5debf7ddaa73d9ad0504677ef69f468fceab171d
ArtificIn <cisspmit@naver.com>
2019-01-03 02:18:08 +0900

    [다연] 일정 추가 창 완료

--- flower/Home/Calendar/CalendarAddVC.swift
+++ flower/Home/Calendar/CalendarAddVC.swift
@@ -161,9 +161,13 @@ extension CalendarAddVC {
     func setPersonView(){
         personDateLabel.text = setDateLabel()
         
-        let eatSegmentControl = BetterSegmentedControl(frame: CGRect(x: (view.frame.width - 302 ) / 2 , y: (view.frame.height)/2 + 14, width: 302, height: 38), segments: LabelSegment.segments(withTitles: ["같이 먹어요!", "먹고 올게요!"], normalBackgroundColor: .white, normalTextColor: UIColor.init(hex: "#656565"), selectedBackgroundColor: .white, selectedTextColor: UIColor.init(hex: "#366CE2")), index: 1, options: [.backgroundColor(UIColor.init(hex: "#656565")), .indicatorViewBackgroundColor(UIColor.init(hex: "#366CE2")) ])
+        let eatSegmentControl = BetterSegmentedControl(frame: CGRect(x: (view.frame.width - 302 ) / 2 - 4, y: (view.frame.height)/2 - 100, width: 306, height: 38), segments: LabelSegment.segments(withTitles: ["같이 먹어요!", "먹고 올게요!"], normalBackgroundColor: .white, normalTextColor: UIColor.init(hex: "#656565"), selectedBackgroundColor: .white, selectedTextColor: UIColor.init(hex: "#366CE2")), index: 1, options: [.backgroundColor(UIColor.init(hex: "#656565")), .indicatorViewBackgroundColor(UIColor.init(hex: "#366CE2")) ])
+//        eatSegmentControl.layer.borderWidth = 3
+//        eatSegmentControl.layer.borderColor = UIColor(hex: "#366CE2").cgColor
         
-        let backSegmentControl = BetterSegmentedControl(frame: CGRect(x: (view.frame.width - 302) / 2, y: (view.frame.height) / 2 - 116.5, width: 302, height: 75), segments: LabelSegment.segments(withTitles: ["10시 전", "10시 ~ 12시", "12시 이후"], normalBackgroundColor: .white, normalTextColor: UIColor.init(hex: "#656565"), selectedBackgroundColor: .white, selectedTextColor: UIColor.init(hex: "#366CE2")), index: 1, options: [.backgroundColor(UIColor.init(hex: "#656565")), .indicatorViewBackgroundColor(UIColor.init(hex: "#366CE2")) ])
+        let backSegmentControl = BetterSegmentedControl(frame: CGRect(x: (view.frame.width - 302) / 2 - 4, y: (view.frame.height) / 2 + 14, width: 306, height: 75), segments: LabelSegment.segments(withTitles: ["10시 전", "10시 ~ 12시", "12시 이후"], normalBackgroundColor: .white, normalTextColor: UIColor.init(hex: "#656565"), selectedBackgroundColor: .white, selectedTextColor: UIColor.init(hex: "#366CE2")), index: 1, options: [.backgroundColor(UIColor.init(hex: "#656565")), .indicatorViewBackgroundColor(UIColor.init(hex: "#366CE2")) ])
+//        backSegmentControl.layer.borderWidth = 3
+//        backSegmentControl.layer.borderColor = UIColor.init(hex: "#366CE2").cgColor
         
         eatSegmentControl.layer.cornerRadius = 5
         backSegmentControl.layer.cornerRadius = 5
@@ -171,6 +175,8 @@ extension CalendarAddVC {
         eatSegmentControl.addTarget(self, action: #selector(CalendarAddVC.personEatSegmentControl(_:)), for: .valueChanged)
         backSegmentControl.addTarget(self, action: #selector(CalendarAddVC.personBackSegmentControl(_:)), for: .valueChanged)
 
+        perseonView.addSubview(eatSegmentControl)
+        perseonView.addSubview(backSegmentControl)
     }
 
     func setFamilyView(){
