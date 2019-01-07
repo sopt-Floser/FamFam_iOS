c9ede402e37afc60bcc81cdf3fd136f33a957447
jimin <kimjiminhy@gmail.com>
2019-01-07 22:01:02 +0900

    [지민] 코드 생성

--- flower/Main/JoinCreateVC.swift
+++ flower/Main/JoinCreateVC.swift
@@ -9,22 +9,38 @@
 import UIKit
 
 class JoinCreateVC: UIViewController {
+    
+    @IBOutlet var codeLabel: UILabel!
+    
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        GroupService.shared.inviteGroup{ jimin in
+            guard let jimin2 = jimin.status else {return}
+            guard let jimin3 = jimin.data else {return}
+            switch jimin2{
+            case 200 :
+                self.codeLabel.text = jimin3.code!
+                print ("아무말")
+            case 400 :
+                print ("초대코드 생성 실패")
+            case 500 :
+                print ("서버 내부 에러")
+            case 600 :
+                print ("DB 에러")
+            default :
+                print ("그룹 초대 코드 생성/조회")
+            }
+            
+        }
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
 
-        // Do any additional setup after loading the view.
     }
-    
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
