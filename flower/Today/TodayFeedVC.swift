f4b2911e32464b37a3108d3ade7ff74ee6cc4d8b
jimin <kimjiminhy@gmail.com>
2019-01-08 19:06:46 +0900

    [지민] 투데이 추가

--- flower/Today/TodayFeedVC.swift
+++ flower/Today/TodayFeedVC.swift
@@ -13,7 +13,10 @@ import UIKit
 
 class TodayFeedVC: UIViewController {
     
+//    @IBOutlet var todayFeedTable: UITableView!
     @IBOutlet var todayFeedTable: UITableView!
+    
+    
     var todayFeedList = [Today_Contents]()
     var cellHeight : CGFloat = 0.0
     override func viewDidLoad() {
@@ -45,11 +48,10 @@ class TodayFeedVC: UIViewController {
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         self.hidesBottomBarWhenPushed = true
-//        TodayService.shared.getBoardList(offset: nil, limit: nil){[weak self] (data) in guard let `self` = self else {return}
-//            self.todayFeedList = data
-//            self.todayFeedTable.reloadData()
-//            
-//        }
+        TodayService.shared.getAllContent(page_no: 5, page_size: 5){[weak self] (data) in guard let `self` = self else {return}
+            self.todayFeedList = data
+            self.todayFeedTable.reloadData()
+        }
     }
     
 }
@@ -70,13 +72,13 @@ extension TodayFeedVC: UITableViewDataSource {
         //각 row에 해당하는 cell의 데이터를 넣어주기위해 모델에서 post 데이터 하나를 선언합니다.
         let post = todayFeedList[indexPath.row]
         
-        /*
-        cell.postName.text = post.content?.userIdx
+        
+        cell.postName.text = String(post.content?.userIdx ?? 0)
         cell.postDate.text = post.content?.createDate
-        cell.postImage?.imageFromUrl(gsno(post.content.photoName), defaultImgPath: "")
+        cell.postImage?.imageFromUrl(gsno(post.photos), defaultImgPath: "")
         cell.postContent.text = post.content?.content
-        cell.replyCount.text = post.content?.commentCount
-        */
+        cell.replyCount.text = String(post.content?.commentCount ?? 0)
+ 
         
         
 //        /** string type의 optional을 풀어준다 */
@@ -163,16 +165,16 @@ extension TodayFeedVC: UITableViewDelegate {
         
         nextVC.viewHeight = cellHeight //셀의 높이를 다음 화면의 뷰 높이로 보냄
         
-        /*
+        
         //nextVC.postProfileImage = post.postProfileImage
-        nextVC.postName = post.content?.userIdx
+        nextVC.postName = String(post.content?.userIdx ?? 0)
         nextVC.postDate = post.content?.createDate
         // 중간 (게시글)
         nextVC.postImage?.imageFromUrl(gsno(post.content.photoName), defaultImgPath: "")
         nextVC.postImagePageControl = post.postImagePagecontrol
         // 중간 (감정)
         
-        */
+        
         
         
         //nextVC.emotionImage = post.emotionImage
@@ -193,12 +195,12 @@ extension TodayFeedVC: UITableViewDelegate {
     
 }
 
-//
 //extension TodayFeedVC {
 //    func getTodayFeedData() {
-//        let post1 = TodayFeedData(pPImage: "sampleProfile", pName: "승수", pDate: "19950801", pImage: "tempImg", pPage: 3, eImage: "emotionSmile", eName: "엄마", pContent: "엄마와 함께 즐거운 데이트! :) 날씨는 별로 엄마는 내 맘의 별로>< 배고프고 춥고 어쩌구 저쩌구!", rCount: 2)
-//        let post2 = TodayFeedData(pPImage: "momPic", pName: "엄마", pDate: "20181225", pImage: "cakeImg", pPage: 5, eImage: "emotionSmile", eName: "막내", pContent: "케이크 먹고싶네", rCount: 4)
-//        todayFeedList = [post1,post2]
+//        let post1 = TodayFeedData(pPImage: "sampleProfile", pName: "승수", pDate: "19950801", pImage: "tempImg", pPage: 3, eImage: "emotionSmile", eName: "엄마", pContent: "스누피 뒷모습 너무 귀엽다 찰리브라운 대머리 스누피 귀가 잘 안보이네", rCount: 2)
+//        let post2 = TodayFeedData(pPImage: "momPic", pName: "엄마", pDate: "20181225", pImage: "cakeImg", pPage: 5, eImage: "emotionSmile", eName: "막내", pContent: "엄마와 함께 즐거운 데이트! :) 날씨는 별로 엄마는 내 맘의 별로>< 배고프고 춥고 어쩌구 저쩌구! 엄청 많이 말하면 줄이 넘어가나 셀 사이즈 조정을 해야하는 데 그건 어떻게 해 망했어 진짜 어쩌구 저쩌구 길어졌나? 엄마와 함께 즐거운 데이트! :) 날씨는 별로 엄마는 내 맘의 별로>< 배고프고 춥고 어쩌구 저쩌구! 엄청 많이 말하면 줄이 넘어가나 셀 사이즈 조정을 해야하는 데 그건 어떻게 해 망했어 진짜 어쩌구 저쩌구 길어졌나? 엄마와 함께 즐거운 데이트! :) 날씨는 별로 엄마는 내 맘의 별로>< 배고프고 춥고 어쩌구 저쩌구! 엄청 많이 말하면 줄이 넘어가나 셀 사이즈 조정을 해야하는 데 그건 어떻게 해 망했어 진짜 어쩌구 저쩌구 길어졌나? 엄마와 함께 즐거운 데이트! :) 날씨는 별로 엄마는 내 맘의 별로>< 배고프고 춥고 어쩌구 저쩌구! 엄청 많이 말하면 줄이 넘어가나 셀 사이즈 조정을 해야하는 데 그건 어떻게 해 망했어 진짜 어쩌구 저쩌구 길어졌나? 엄마와 함께 즐거운 데이트! :) 날씨는 별로 엄마는 내 맘의 별로>< 배고프고 춥고 어쩌구 저쩌구! 엄청 많이 말하면 줄이 넘어가나 셀 사이즈 조정을 해야하는 데 그건 어떻게 해 망했어 진짜 어쩌구 저쩌구 길어졌나? 엄마와 함께 즐거운 데이트! :) 날씨는 별로 엄마는 내 맘의 별로>< 배고프고 춥고 어쩌구 저쩌구! 엄청 많이 말하면 줄이 넘어가나 셀 사이즈 조정을 해야하는 데 그건 어떻게 해 망했어 진짜 어쩌구 저쩌구 길어졌나? ", rCount: 4)
+//        let post3 = TodayFeedData(pPImage: "tempImg", pName: "스누피", pDate: "20181225", pImage: "family", pPage: 5, eImage: "emotionSmile", eName: "막내", pContent: "가족", rCount: 10)
+//        todayFeedList = [post1,post2,post3]
 //
 //    }
 //
